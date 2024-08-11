d792c1a2392c9732886305104ae53fcc8c26c00f
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-08-10 20:23:33 -0400

    sort departments list

--- booking-app/components/src/client/routes/admin/components/Departments.tsx
+++ booking-app/components/src/client/routes/admin/components/Departments.tsx
@@ -1,19 +1,20 @@
-import React, { useContext, useMemo, useState } from 'react';
-import AddRow from '../../components/AddRow';
-import { Box } from '@mui/material';
-import { DatabaseContext } from '../../components/Provider';
-import { Department } from '../../../../types';
-import Dropdown from '../../booking/components/Dropdown';
-import ListTable from '../../components/ListTable';
-import { formatDate } from '../../../utils/date';
-import { getLiaisonTableName, TableNames } from '../../../../policy';
-import AddDepartmentRow from '../../components/AddDepartmentRow';
+import React, { useContext, useMemo, useState } from "react";
+import { TableNames, getLiaisonTableName } from "../../../../policy";
+
+import AddDepartmentRow from "../../components/AddDepartmentRow";
+import AddRow from "../../components/AddRow";
+import { Box } from "@mui/material";
+import { DatabaseContext } from "../../components/Provider";
+import { Department } from "../../../../types";
+import Dropdown from "../../booking/components/Dropdown";
+import ListTable from "../../components/ListTable";
+import { formatDate } from "../../../utils/date";
 
 const AddDepartmentForm = ({ departments, reloadDepartments }) => {
   const [departmentName, setDepartmentName] = useState("");
   const [departmentTier, setDepartmentTier] = useState("");
 
-  const departmentDropdown = useMemo( 
+  const departmentDropdown = useMemo(
     () => (
       <Box width="200px" marginLeft={1}>
         <Dropdown
@@ -27,14 +28,13 @@ const AddDepartmentForm = ({ departments, reloadDepartments }) => {
     [departmentName]
   );
 
-
   const departmentTierDropdown = useMemo(
     () => (
       <Box width="200px" marginLeft={1}>
         <Dropdown
           value={departmentTier}
           updateValue={setDepartmentTier}
-          options={['Primary', 'Secondary', 'Tertiary']} 
+          options={["Primary", "Secondary", "Tertiary"]}
           placeholder="Choose a Tier"
         />
       </Box>
@@ -47,19 +47,20 @@ const AddDepartmentForm = ({ departments, reloadDepartments }) => {
       addDuplicateErrorMessage="This department is already registered"
       addFailedErrorMessage="Failed to add Department"
       columnNameUniqueValue="department"
-      valueToAdd = {departmentName}
+      valueToAdd={departmentName}
       tableName={TableNames.DEPARTMENTS}
       rows={departments}
       rowsRefresh={reloadDepartments}
       title="Departments"
-      components = {[departmentDropdown, departmentTierDropdown]}
-      values = {{departmentName, departmentTier}}
+      components={[departmentDropdown, departmentTierDropdown]}
+      values={{ departmentName, departmentTier }}
     />
   );
 };
 
 export const Departments = () => {
-  const { departmentNames, reloadDepartmentNames } = useContext(DatabaseContext);
+  const { departmentNames, reloadDepartmentNames } =
+    useContext(DatabaseContext);
 
   const departments = useMemo<string[]>(
     () => departmentNames.map((user) => user.department),
@@ -71,14 +72,15 @@ export const Departments = () => {
   };
 
   const formattedDepartmentNames = useMemo(() => {
-    return departmentNames.map((user) => {
-      return {
-        ...user,
-        departmentTier: user.departmentTier || "Unknown", // Display empty string if departmentTier is falsy
-      };
-    });
+    return departmentNames
+      .map((user) => {
+        return {
+          ...user,
+          departmentTier: user.departmentTier || "Unknown", // Display empty string if departmentTier is falsy
+        };
+      })
+      .sort((a, b) => a.department.localeCompare(b.department));
   }, [departmentNames]);
-  
 
   return (
     <ListTable
@@ -92,7 +94,10 @@ export const Departments = () => {
           reloadDepartments={reloadDepartmentNames}
         />
       }
-      columnFormatters={{ createdAt: formatDate, departmentTier: formatDepartmentTier }} // Ensure columnFormatters includes departmentTier
+      columnFormatters={{
+        createdAt: formatDate,
+        departmentTier: formatDepartmentTier,
+      }} // Ensure columnFormatters includes departmentTier
     />
   );
 };
