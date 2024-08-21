6863920885460802c986ff7b6d0b654dc2373d72
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-08-21 10:17:07 -0700

    DELETE OLD REPO

--- media_commons_booking_app/src/client/routes/admin/adminPage.jsx
+++
@@ -1,6 +0,0 @@
-import Admin from './components/Admin';
-import React from 'react';
-
-export default function AdminPage() {
-  return <Admin />;
-}
--- media_commons_booking_app/src/client/routes/myBookings/myBookingsPage.tsx
+++
@@ -1,6 +0,0 @@
-import { Bookings } from '../admin/components/Bookings';
-import React from 'react';
-
-export default function MyBookingsPage() {
-  return <Bookings isUserView={true} />;
-}
