450a3cce33978c9b9922efd23840a04eb207bc5c
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-10-01 20:15:06 -0400

    don't fetch calendar events until roomSettings load

--- booking-app/components/src/client/routes/booking/hooks/fetchCalendarEvents.tsx
+++ booking-app/components/src/client/routes/booking/hooks/fetchCalendarEvents.tsx
@@ -2,12 +2,14 @@ import { CalendarEvent, RoomSetting } from "../../../../types";
 import { useCallback, useEffect, useState } from "react";
 
 import { CALENDAR_HIDE_STATUS } from "../../../../policy";
-import axios from "axios";
 
 export default function fetchCalendarEvents(allRooms: RoomSetting[]) {
   const [events, setEvents] = useState<CalendarEvent[]>([]);
 
   const loadEvents = useCallback(() => {
+    if (allRooms.length === 0) {
+      return;
+    }
     Promise.all(allRooms.map(fetchRoomCalendarEvents)).then((results) => {
       const flatResults = results.flat();
       console.log("FETCHED CALENDAR RESULTS:", flatResults.length);
@@ -31,13 +33,17 @@ export default function fetchCalendarEvents(allRooms: RoomSetting[]) {
 
   const fetchRoomCalendarEvents = async (room: RoomSetting) => {
     const calendarId = room.calendarId;
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/calendarEvents`,
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/calendarEvents?calendarId=${calendarId}`,
       {
-        params: { calendarId: calendarId },
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
     );
-    const filteredEvents = response.data.filter((row: any) => {
+    const data = await response.json();
+
+    const filteredEvents = data.filter((row: any) => {
       return !CALENDAR_HIDE_STATUS.some((status) =>
         row?.title?.includes(status)
       );
@@ -49,24 +55,6 @@ export default function fetchCalendarEvents(allRooms: RoomSetting[]) {
     }));
     return rowsWithResourceIds;
   };
-
-  // TODO add this back or delete it
-  // const getFakeEvents: () => CalendarEvent[] = () => {
-  //   const existingFakeData = localStorage.getItem(STORAGE_KEY_BOOKING);
-  //   if (existingFakeData != null && process.env.BRANCH_NAME === "development") {
-  //     const json = JSON.parse(existingFakeData);
-  //     return json.bookingRows.map((booking: Booking) => ({
-  //       title: `[${getBookingStatus(booking, json.bookingStatusRows)}] ${
-  //         booking.title
-  //       }`,
-  //       start: booking.startDate,
-  //       end: booking.endDate,
-  //       id: booking.roomId,
-  //       resourceId: booking.roomId,
-  //     }));
-  //   }
-  //   return [];
-  // };
 
   return {
     existingCalendarEvents: events,
