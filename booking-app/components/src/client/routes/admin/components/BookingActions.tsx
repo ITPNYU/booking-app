9afe8ef59961a4c61d6eedd3b4ed9098e9468e6c
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-10-08 16:14:09 -0400

    refactor booking actions component

--- booking-app/components/src/client/routes/admin/components/BookingActions.tsx
+++ booking-app/components/src/client/routes/admin/components/BookingActions.tsx
@@ -1,24 +1,17 @@
 import { BookingStatusLabel, PageContextLevel } from "../../../../types";
 import { IconButton, MenuItem, Select } from "@mui/material";
-import React, { useContext, useMemo, useState } from "react";
-import {
-  cancel,
-  checkOut,
-  checkin,
-  clientApproveBooking,
-  decline,
-  noShow,
-} from "@/components/src/server/db";
+import React, { useContext, useState } from "react";
+import useBookingActions, {
+  ActionDefinition,
+  Actions,
+} from "../hooks/useBookingActions";
 
 import AlertToast from "../../components/AlertToast";
-import { BookingContext } from "../../booking/bookingProvider";
 import Check from "@mui/icons-material/Check";
 import ConfirmDialog from "../../components/ConfirmDialog";
 import { DatabaseContext } from "../../components/Provider";
 import Loading from "../../components/Loading";
 import { Timestamp } from "@firebase/firestore";
-import useExistingBooking from "../hooks/useExistingBooking";
-import { useRouter } from "next/navigation";
 
 interface Props {
   calendarEventId: string;
@@ -28,43 +21,26 @@ interface Props {
   startDate: Timestamp;
 }
 
-enum Actions {
-  CANCEL = "Cancel",
-  NO_SHOW = "No Show",
-  CHECK_IN = "Check In",
-  CHECK_OUT = "Check Out",
-  FIRST_APPROVE = "1st Approve",
-  FINAL_APPROVE = "Final Approve",
-  DECLINE = "Decline",
-  EDIT = "Edit",
-  MODIFICATION = "Modification",
-  PLACEHOLDER = "",
-}
-
-type ActionDefinition = {
-  // TODO: Fix this type
-  action: () => any;
-  optimisticNextStatus: BookingStatusLabel;
-  confirmation?: boolean;
-};
-
-export default function BookingActions({
-  status,
-  calendarEventId,
-  pageContext,
-  setOptimisticStatus,
-  startDate,
-}: Props) {
+export default function BookingActions(props: Props) {
+  const {
+    status,
+    calendarEventId,
+    pageContext,
+    setOptimisticStatus,
+    startDate,
+  } = props;
   const [uiLoading, setUiLoading] = useState(false);
   const [selectedAction, setSelectedAction] = useState<Actions>(
     Actions.PLACEHOLDER
   );
   const { reloadBookings, reloadBookingStatuses } = useContext(DatabaseContext);
-  const { reloadExistingCalendarEvents } = useContext(BookingContext);
   const [showError, setShowError] = useState(false);
-  const [date, setDate] = useState(new Date());
-  const router = useRouter();
-  const loadExistingBookingData = useExistingBooking();
+  const { actions, updateActions, options } = useBookingActions({
+    status,
+    calendarEventId,
+    pageContext,
+    startDate,
+  });
 
   const reload = async () => {
     await Promise.all([reloadBookings(), reloadBookingStatuses()]);
@@ -72,10 +48,6 @@ export default function BookingActions({
 
   const onError = () => {
     setShowError(true);
-  };
-
-  const updateActions = () => {
-    setDate(new Date());
   };
 
   const handleDialogChoice = (result: boolean) => {
@@ -103,148 +75,6 @@ export default function BookingActions({
       setOptimisticStatus(undefined);
       setSelectedAction(Actions.PLACEHOLDER);
       setUiLoading(false);
-    }
-  };
-
-  const actions: { [key in Actions]: ActionDefinition } = {
-    [Actions.CANCEL]: {
-      action: async () => {
-        await cancel(calendarEventId);
-      },
-      optimisticNextStatus: BookingStatusLabel.CANCELED,
-      confirmation: true,
-    },
-    [Actions.NO_SHOW]: {
-      action: async () => {
-        await noShow(calendarEventId);
-      },
-      optimisticNextStatus: BookingStatusLabel.NO_SHOW,
-    },
-    [Actions.CHECK_IN]: {
-      action: async () => {
-        await checkin(calendarEventId);
-      },
-      optimisticNextStatus: BookingStatusLabel.CHECKED_IN,
-    },
-    [Actions.CHECK_OUT]: {
-      action: async () => {
-        await checkOut(calendarEventId);
-      },
-      optimisticNextStatus: BookingStatusLabel.CHECKED_OUT,
-    },
-    [Actions.FIRST_APPROVE]: {
-      action: async () => {
-        await clientApproveBooking(calendarEventId);
-      },
-      optimisticNextStatus: BookingStatusLabel.PENDING,
-    },
-    [Actions.FINAL_APPROVE]: {
-      action: async () => {
-        await clientApproveBooking(calendarEventId);
-      },
-      optimisticNextStatus: BookingStatusLabel.APPROVED,
-    },
-    [Actions.DECLINE]: {
-      action: async () => {
-        await decline(calendarEventId);
-      },
-      optimisticNextStatus: BookingStatusLabel.DECLINED,
-      confirmation: true,
-    },
-    [Actions.EDIT]: {
-      action: async () => {
-        loadExistingBookingData(calendarEventId);
-        reloadExistingCalendarEvents();
-        router.push("/edit/" + calendarEventId);
-      },
-      optimisticNextStatus: status,
-      confirmation: false,
-    },
-    [Actions.MODIFICATION]: {
-      action: async () => {
-        loadExistingBookingData(calendarEventId);
-        reloadExistingCalendarEvents();
-        router.push("/modification/" + calendarEventId);
-      },
-      optimisticNextStatus: status,
-      confirmation: false,
-    },
-    // never used, just make typescript happy
-    [Actions.PLACEHOLDER]: {
-      action: async () => {},
-      optimisticNextStatus: BookingStatusLabel.UNKNOWN,
-    },
-  };
-
-  const userOptions = useMemo(() => {
-    let options = [];
-    if (status !== BookingStatusLabel.CANCELED) {
-      options.push(Actions.CANCEL);
-    }
-    if (
-      status !== BookingStatusLabel.CHECKED_IN &&
-      status !== BookingStatusLabel.NO_SHOW &&
-      startDate.toDate() > date
-    ) {
-      options.push(Actions.EDIT);
-    }
-    return options;
-  }, [status]);
-
-  const paOptions = useMemo(() => {
-    let options = [];
-
-    if (status === BookingStatusLabel.APPROVED) {
-      options.push(Actions.CHECK_IN);
-      options.push(Actions.NO_SHOW);
-      options.push(Actions.MODIFICATION);
-    } else if (status === BookingStatusLabel.CHECKED_IN) {
-      options.push(Actions.NO_SHOW);
-      options.push(Actions.CHECK_OUT);
-      options.push(Actions.MODIFICATION);
-    } else if (status === BookingStatusLabel.NO_SHOW) {
-      options.push(Actions.CHECK_IN);
-    } else if (status === BookingStatusLabel.WALK_IN) {
-      options.push(Actions.CHECK_OUT);
-      options.push(Actions.MODIFICATION);
-    }
-    return options;
-  }, [status]);
-
-  const liaisonOptions = [Actions.FIRST_APPROVE, Actions.DECLINE];
-
-  const adminOptions = useMemo(() => {
-    if (
-      status === BookingStatusLabel.CANCELED ||
-      status === BookingStatusLabel.DECLINED ||
-      status === BookingStatusLabel.CHECKED_OUT
-    ) {
-      return [];
-    }
-
-    let options: Actions[] = [];
-    if (status === BookingStatusLabel.REQUESTED) {
-      options.push(Actions.FIRST_APPROVE);
-    } else if (status === BookingStatusLabel.PENDING) {
-      options.push(Actions.FINAL_APPROVE);
-    }
-
-    options = options.concat(paOptions);
-    options.push(Actions.CANCEL);
-    options.push(Actions.DECLINE);
-    return options;
-  }, [status, paOptions, date]);
-
-  const options = () => {
-    switch (pageContext) {
-      case PageContextLevel.USER:
-        return userOptions;
-      case PageContextLevel.PA:
-        return paOptions;
-      case PageContextLevel.LIAISON:
-        return liaisonOptions;
-      default:
-        return adminOptions;
     }
   };
 
