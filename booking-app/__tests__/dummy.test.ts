a77cf71bb70ea25a395e1746e767238b355731a4
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-10-13 14:14:53 -0700

    dummy unit tests

---
+++ booking-app/__tests__/dummy.test.ts
@@ -0,0 +1,9 @@
+describe("Dummy TypeScript test suite", () => {
+  test("This test will purposely fail", () => {
+    expect(true).toBe(false);
+  });
+
+  test("This test will trivially pass", () => {
+    expect(true).toBe(true);
+  });
+});
