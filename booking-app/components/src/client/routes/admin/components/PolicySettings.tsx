4ac2f7fa3b62de9e5b1163f518717f9e5c4b3652
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-11-19 15:19:18 -0500

    operational hours UI

--- booking-app/components/src/client/routes/admin/components/PolicySettings.tsx
+++ booking-app/components/src/client/routes/admin/components/PolicySettings.tsx
@@ -1,11 +1,12 @@
 import { Box } from "@mui/material";
 import FinalApprover from "./policySettings/FinalApprover";
-import React from "react";
+import OperationalHours from "./OperationalHours";
 
 export default function PolicySettings() {
   return (
     <Box>
       <FinalApprover />
+      <OperationalHours />
     </Box>
   );
 }
