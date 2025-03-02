bbbbd80830dc6c797f8a1056ddf41bc7fd2d36b6
nimanns <qwerty.nma@gmail.com>
2025-03-02 17:32:30 -0500

    add search functionality

--- booking-app/components/src/client/routes/components/bookingTable/hooks/useBookingFilters.ts
+++ booking-app/components/src/client/routes/components/bookingTable/hooks/useBookingFilters.ts
@@ -19,6 +19,7 @@ interface Props {
   columnOrder: ColumnSortOrder;
   selectedDateRange: DateRangeFilter;
   selectedStatusFilters: BookingStatusLabel[];
+  searchQuery?: string;
 }
 
 function getDateRangeFromDateSelection(selectedDateRange: DateRangeFilter) {
@@ -109,158 +110,193 @@ function getDateRangeFromDateSelection(selectedDateRange: DateRangeFilter) {
   }
 }
 
-  export function useBookingFilters(props: Props): BookingRow[] {
-    const {
-      pageContext,
-      columnOrderBy,
-      columnOrder,
-      selectedDateRange,
-      selectedStatusFilters,
-    } = props;
-    const { liaisonUsers, userEmail, allBookings, setFilters } = useContext(DatabaseContext);
-    const allowedStatuses = useAllowedStatuses(pageContext);
-    const [currentTime, setCurrentTime] = useState(new Date());
+class BookingFilter {
+  rows: BookingRow[];
+  allowedStatuses: BookingStatusLabel[];
+  pageContext: PageContextLevel;
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentTime(new Date());
-      }, 60000); // Update every minute
-
-      return () => clearInterval(interval);
-    }, []);
-
-    useEffect(() => {
-      console.log("selectedDateRange",getDateRangeFromDateSelection(selectedDateRange));
-      setFilters({
-        dateRange: getDateRangeFromDateSelection(selectedDateRange),
-        sortField: "startDate",
-      });
-    }, [selectedDateRange]);
-
-    const rows: BookingRow[] = useMemo(
-      () => {
-        /* return futureBookings.map((booking) => ({
-          ...booking,
-          status: getBookingStatus(booking),
-        }))} */
-        return allBookings.map((booking) => ({
-          ...booking,
-          status: getBookingStatus(booking),
-        }))
-      },
-      [allBookings]
-    );
-
-    const filteredRows = useMemo(() => {
-      const filter = new BookingFilter({
-        rows,
-        allowedStatuses,
-        pageContext,
-      });
-
-      return filter
-        // .filterSelectedDateRange(selectedDateRange)
-        .filterElapsedTime(currentTime)
-        .filterPageContext(userEmail, liaisonUsers)
-        .filterAllowedStatuses()
-        .filterStatusChips(selectedStatusFilters)
-        .sortByColumn(columnOrderBy, columnOrder)
-        .getRows();
-    }, [
-      rows,
-      columnOrderBy,
-      columnOrder,
-      currentTime,
-      pageContext,
-      // selectedDateRange,
-      selectedStatusFilters,
-    ]);
-
-    return filteredRows;
-  }
-
-  class BookingFilter {
+  constructor(obj: {
     rows: BookingRow[];
     allowedStatuses: BookingStatusLabel[];
     pageContext: PageContextLevel;
-
-    constructor(obj: {
-      rows: BookingRow[];
-      allowedStatuses: BookingStatusLabel[];
-      pageContext: PageContextLevel;
-    }) {
-      this.rows = obj.rows;
-      this.allowedStatuses = obj.allowedStatuses;
-      this.pageContext = obj.pageContext;
-    }
-
-    // filter if endTime has passed and status should be hidden
-    // checks once per minute
-    filterElapsedTime(currentTime: Date) {
-      this.rows = this.rows.filter(
-        (row) =>
-          !(
-            BOOKING_TABLE_HIDE_STATUS_TIME_ELAPSED.includes(row.status) &&
-            row.endDate.toDate() < currentTime
-          )
-      );
-      return this;
-    }
-
-    filterPageContext(userEmail: string, liaisonUsers: Approver[]) {
-      if (this.pageContext === PageContextLevel.USER) {
-        this.rows = this.rows.filter((row) => row.email === userEmail);
-      }
-      if (this.pageContext === PageContextLevel.LIAISON) {
-        const liaisonMatches = liaisonUsers.filter(
-          (user) => user.email === userEmail
-        );
-        if (liaisonMatches.length > 0) {
-          const liaisonDepartments = liaisonMatches.map(
-            (user) => user.department
-          );
-          this.rows = this.rows.filter((row) =>
-            liaisonDepartments.includes(row.department)
-          );
-        }
-      }
-      return this;
-    }
-
-    filterAllowedStatuses() {
-      this.rows = this.rows.filter((row) =>
-        this.allowedStatuses.includes(row.status)
-      );
-      return this;
-    }
-
-    filterSelectedDateRange(selectedDateRange: DateRangeFilter) {
-      if (this.pageContext >= PageContextLevel.PA) {
-        this.rows = this.rows.filter(DATE_FILTERS[selectedDateRange]);
-      }
-      return this;
-    }
-
-    filterStatusChips(selectedStatusFilters: BookingStatusLabel[]) {
-      if (selectedStatusFilters.length > 0) {
-        this.rows = this.rows.filter((row) =>
-          selectedStatusFilters.includes(row.status)
-        );
-      }
-      return this;
-    }
-
-    sortByColumn(orderBy: keyof BookingRow, order: ColumnSortOrder) {
-      const comparator = COMPARATORS[orderBy];
-      const coeff = order === "asc" ? 1 : -1;
-      if (comparator != null) {
-        this.rows.sort((a, b) => coeff * comparator(a, b));
-      }
-
-      return this;
-    }
-
-    getRows() {
-      return this.rows;
-    }
+  }) {
+    this.rows = obj.rows;
+    this.allowedStatuses = obj.allowedStatuses;
+    this.pageContext = obj.pageContext;
   }
+
+  // filter if endTime has passed and status should be hidden
+  // checks once per minute
+  filterElapsedTime(currentTime: Date) {
+    this.rows = this.rows.filter(
+      (row) =>
+        !(
+          BOOKING_TABLE_HIDE_STATUS_TIME_ELAPSED.includes(row.status) &&
+          row.endDate.toDate() < currentTime
+        )
+    );
+    return this;
+  }
+
+  filterPageContext(userEmail: string, liaisonUsers: Approver[]) {
+    if (this.pageContext === PageContextLevel.USER) {
+      this.rows = this.rows.filter((row) => row.email === userEmail);
+    }
+    if (this.pageContext === PageContextLevel.LIAISON) {
+      const liaisonMatches = liaisonUsers.filter(
+        (user) => user.email === userEmail
+      );
+      if (liaisonMatches.length > 0) {
+        const liaisonDepartments = liaisonMatches.map(
+          (user) => user.department
+        );
+        this.rows = this.rows.filter((row) =>
+          liaisonDepartments.includes(row.department)
+        );
+      }
+    }
+    return this;
+  }
+
+  filterAllowedStatuses() {
+    this.rows = this.rows.filter((row) =>
+      this.allowedStatuses.includes(row.status)
+    );
+    return this;
+  }
+
+  filterSelectedDateRange(selectedDateRange: DateRangeFilter) {
+    if (this.pageContext >= PageContextLevel.PA) {
+      this.rows = this.rows.filter(DATE_FILTERS[selectedDateRange]);
+    }
+    return this;
+  }
+
+  filterStatusChips(selectedStatusFilters: BookingStatusLabel[]) {
+    if (selectedStatusFilters.length > 0) {
+      this.rows = this.rows.filter((row) =>
+        selectedStatusFilters.includes(row.status)
+      );
+    }
+    return this;
+  }
+
+  filterBySearchQuery(searchQuery: string) {
+    if (searchQuery && searchQuery.trim() !== "") {
+      const query = searchQuery.toLowerCase().trim();
+      this.rows = this.rows.filter((row) => {
+        return (
+          // Search in request number
+          (row.requestNumber && row.requestNumber.toString().includes(query)) ||
+          // Search in department
+          (row.department && row.department.toLowerCase().includes(query)) ||
+          // Search in netId/email
+          (row.netId && row.netId.toLowerCase().includes(query)) ||
+          (row.email && row.email.toLowerCase().includes(query)) ||
+          // Search in title
+          (row.title && row.title.toLowerCase().includes(query)) ||
+          // Search in description
+          (row.description && row.description.toLowerCase().includes(query)) ||
+          // Search in name
+          (row.firstName && row.firstName.toLowerCase().includes(query)) ||
+          (row.lastName && row.lastName.toLowerCase().includes(query)) ||
+          // Search in roomId
+          (row.roomId && row.roomId.toLowerCase().includes(query))
+        );
+      });
+    }
+    return this;
+  }
+
+  sortByColumn(orderBy: keyof BookingRow, order: ColumnSortOrder) {
+    const comparator = COMPARATORS[orderBy];
+    const coeff = order === "asc" ? 1 : -1;
+    if (comparator != null) {
+      this.rows.sort((a, b) => coeff * comparator(a, b));
+    }
+
+    return this;
+  }
+
+  getRows() {
+    return this.rows;
+  }
+}
+
+export function useBookingFilters(props: Props): BookingRow[] {
+  const {
+    pageContext,
+    columnOrderBy,
+    columnOrder,
+    selectedDateRange,
+    selectedStatusFilters,
+    searchQuery = "",
+  } = props;
+  const { liaisonUsers, userEmail, allBookings, setFilters } = useContext(DatabaseContext);
+  const allowedStatuses = useAllowedStatuses(pageContext);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 60000); // Update every minute
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    console.log("selectedDateRange",getDateRangeFromDateSelection(selectedDateRange));
+    setFilters({
+      dateRange: getDateRangeFromDateSelection(selectedDateRange),
+      sortField: "startDate",
+      searchQuery: searchQuery,
+    });
+  }, [selectedDateRange, setFilters, searchQuery]);
+
+  const rows: BookingRow[] = useMemo(
+    () => {
+      /* return futureBookings.map((booking) => ({
+        ...booking,
+        status: getBookingStatus(booking),
+      }))} */
+      return allBookings.map((booking) => ({
+        ...booking,
+        status: getBookingStatus(booking),
+      }))
+    },
+    [allBookings]
+  );
+
+  const filteredRows = useMemo(() => {
+    const filter = new BookingFilter({
+      rows,
+      allowedStatuses,
+      pageContext,
+    });
+
+    return filter
+      // .filterSelectedDateRange(selectedDateRange)
+      .filterElapsedTime(currentTime)
+      .filterPageContext(userEmail, liaisonUsers)
+      .filterAllowedStatuses()
+      .filterStatusChips(selectedStatusFilters)
+      // No need for client-side search filtering since it's done on the database side
+      // .filterBySearchQuery(searchQuery)
+      .sortByColumn(columnOrderBy, columnOrder)
+      .getRows();
+  }, [
+    rows,
+    columnOrderBy,
+    columnOrder,
+    currentTime,
+    pageContext,
+    // selectedDateRange,
+    selectedStatusFilters,
+    // searchQuery, // Removed as it's handled by the database
+    userEmail,
+    liaisonUsers,
+    allowedStatuses,
+  ]);
+
+  return filteredRows;
+}
