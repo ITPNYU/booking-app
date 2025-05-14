b8e0fd1e0ade82946ac4d79897a3c38a8563e6c5
riho.takagi <rihohearts@gmail.com>
2025-05-15 04:26:17 +0900

    Record booking logs

--- booking-app/components/src/client/routes/myBookings/myBookingsPage.tsx
+++ booking-app/components/src/client/routes/myBookings/myBookingsPage.tsx
@@ -1,9 +1,8 @@
 import { Box, Typography } from "@mui/material";
 
-import { Bookings } from "../components/bookingTable/Bookings";
 import { PageContextLevel } from "@/components/src/types";
-import React from "react";
 import { styled } from "@mui/system";
+import { Bookings } from "../components/bookingTable/Bookings";
 
 const Center = styled(Box)`
   display: flex;
@@ -20,7 +19,9 @@ export default function MyBookingsPage() {
   return (
     <Center>
       <Box width={{ xs: "90%", md: "65%" }} margin={6}>
-        <Typography variant="h6">Welcome to the Media Commons booking tool!</Typography>
+        <Typography variant="h6">
+          Welcome to the Media Commons booking tool!
+        </Typography>
         <Bookings pageContext={PageContextLevel.USER} />
       </Box>
     </Center>
