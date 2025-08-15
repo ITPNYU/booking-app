c0ebad4484c627f1af28ec2f6014bc6ab4ae3e80
riho.takagi <rihohearts@gmail.com>
2025-08-15 08:58:12 -0400

    Use Xstate for ITP approval flow

--- booking-app/app/[tenant]/layout.tsx
+++ booking-app/app/[tenant]/layout.tsx
@@ -2,7 +2,7 @@ import ClientProvider from "@/components/src/client/routes/components/ClientProv
 import NavBar from "@/components/src/client/routes/components/navBar";
 import { SchemaContextType } from "@/components/src/client/routes/components/SchemaProvider";
 import SchemaProviderWrapper from "@/components/src/client/routes/components/SchemaProviderWrapper";
-import { ALLOWED_TENANTS } from "@/components/src/constants/tenants";
+import { isValidTenant } from "@/components/src/constants/tenants";
 import { TableNames } from "@/components/src/policy";
 import { serverGetDocumentById } from "@/lib/firebase/server/adminDb";
 import { notFound } from "next/navigation";
@@ -16,7 +16,7 @@ type LayoutProps = {
 };
 
 const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
-  if (!ALLOWED_TENANTS.includes(params.tenant as any)) {
+  if (!isValidTenant(params.tenant)) {
     notFound();
   }
 
