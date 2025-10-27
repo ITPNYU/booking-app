86180ffe1066e3eb3e50d3613f309adb9c1dbe98
pearmini <subairui@icloud.com>
2025-10-27 09:43:09 -0400

    Approved booking is not editable

--- booking-app/components/src/client/routes/admin/components/BookingActions.tsx
+++ booking-app/components/src/client/routes/admin/components/BookingActions.tsx
@@ -152,18 +152,15 @@ export default function BookingActions(props: Props) {
       allBookings,
     });
     const disabled = shouldDisable ? [Actions.CHECK_IN] : [];
-    
+
     // Disable Edit action for users when status is not REQUESTED or DECLINED, or when booking is in the past
     if (pageContext === PageContextLevel.USER) {
-      const isEditDisabled = 
-        status !== BookingStatusLabel.REQUESTED && 
-        status !== BookingStatusLabel.DECLINED;
-      
+      const isEditDisabled = status !== BookingStatusLabel.DECLINED;
       if (isEditDisabled) {
         disabled.push(Actions.EDIT);
       }
     }
-    
+
     return disabled;
   }, [pageContext, startDate, allBookings, calendarEventId, status]);
 
