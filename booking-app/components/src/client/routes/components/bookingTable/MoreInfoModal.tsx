f194f1bf97e336237839a1d7013e16d14a95d6d0
n3xta <ml9389@nyu.edu>
2025-11-23 22:35:06 -0500

    Update date formatting in MoreInfoModal to use "en-US" locale and adjust test cases for new date format

--- booking-app/components/src/client/routes/components/bookingTable/MoreInfoModal.tsx
+++ booking-app/components/src/client/routes/components/bookingTable/MoreInfoModal.tsx
@@ -433,7 +433,7 @@ export default function MoreInfoModal({
             </RoomDetails>
             <RoomDetails container>
               <label>Date:</label>
-              <p>{booking.startDate.toDate().toLocaleDateString()}</p>
+              <p>{booking.startDate.toDate().toLocaleDateString("en-US")}</p>
             </RoomDetails>
             <RoomDetails container>
               <label>Time:</label>
