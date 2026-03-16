b6a395645a3391670cb715de986251e21c481fe8
pearmini <subairui@icloud.com>
2026-03-15 22:55:18 -0400

    Add spaces between tables

--- booking-app/components/src/client/routes/admin/components/Approvers.tsx
+++ booking-app/components/src/client/routes/admin/components/Approvers.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { EquipmentUsers } from "./EquipmentUsers";
 import { Liaisons } from "./Liaisons";
 import { ServiceApproverUsers } from "./ServiceApproverUsers";
@@ -16,11 +16,13 @@ export const Approvers = () => (
     <Typography style={{ marginTop: 48, marginBottom: 24 }} variant="h6">
       Service Approvers
     </Typography>
-    <ServiceApproverUsers title="Setup Approvers" flagField="isSetup" />
-    <ServiceApproverUsers title="Equipment Approvers" flagField="isEquipment" />
-    <ServiceApproverUsers title="Staffing Approvers" flagField="isStaffing" />
-    <ServiceApproverUsers title="Catering Approvers" flagField="isCatering" />
-    <ServiceApproverUsers title="Cleanup Approvers" flagField="isCleaning" />
-    <ServiceApproverUsers title="Security Approvers" flagField="isSecurity" />
+    <Stack spacing={4}>
+      <ServiceApproverUsers title="Setup Approvers" flagField="isSetup" />
+      <ServiceApproverUsers title="Equipment Approvers" flagField="isEquipment" />
+      <ServiceApproverUsers title="Staffing Approvers" flagField="isStaffing" />
+      <ServiceApproverUsers title="Catering Approvers" flagField="isCatering" />
+      <ServiceApproverUsers title="Cleanup Approvers" flagField="isCleaning" />
+      <ServiceApproverUsers title="Security Approvers" flagField="isSecurity" />
+    </Stack>
   </div>
 );
