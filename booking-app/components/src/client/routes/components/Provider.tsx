fc172d7a902564f63dea3f410afdb2c9b255be5e
Riho Takagi <rihohearts@gmail.com>
2026-04-06 14:28:18 -0500

    Revert "fix: improve /mc initial load performance"

--- booking-app/components/src/client/routes/components/Provider.tsx
+++ booking-app/components/src/client/routes/components/Provider.tsx
@@ -222,17 +222,28 @@ export const DatabaseProvider = ({
     if (paEmails.includes(userEmail)) return PagePermission.PA;
 
     return PagePermission.BOOKING;
-  }, [userEmail, adminUsers, liaisonUsers, paUsers, equipmentUsers, superAdminUsers]);
+  }, [
+    userEmail,
+    // Make sure we're using the actual arrays in dependencies
+    JSON.stringify(adminUsers),
+    JSON.stringify(liaisonUsers),
+    JSON.stringify(paUsers),
+    JSON.stringify(equipmentUsers),
+    JSON.stringify(superAdminUsers),
+  ]);
 
 
   useEffect(() => {
     if (!bookingsLoading && tenant) {
       fetchSafetyTrainedUsers();
       fetchBannedUsers();
+      fetchApproverUsers();
       fetchDepartmentNames();
       fetchSettings();
+    } else {
+      // fetchBookings();
     }
-  }, [bookingsLoading, tenant]);
+  }, [bookingsLoading, user, tenant]);
 
   useEffect(() => {
     fetchBookings();
